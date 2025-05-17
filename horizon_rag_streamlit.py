e81289fcf6a2ca34b00e87db4011063a30701d79
Rkiss97 <167915036+Rkiss97@users.noreply.github.com>
2025-05-17 09:33:37 +0200

    Update horizon_rag_streamlit.py

--- horizon_rag_streamlit.py
+++ horizon_rag_streamlit.py
@@ -2,7 +2,7 @@ import streamlit as st
 from langchain.vectorstores import FAISS
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
 import os
 
 # Set API key from Streamlit secrets
